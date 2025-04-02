9dead633d211fd1d9e1c7341e856a946965085eb
Daidanny008 <59802789+Daidanny008@users.noreply.github.com>
2025-04-02 01:59:26 -0400

    Start of Assignment 3, added testing class, modified pom file to use JUnit, no main code modified

---
+++ src/tests/java/ca/mcmaster/se2aa4/mazerunner/TestMazeRunner.java
@@ -0,0 +1,11 @@
+package ca.mcmaster.se2aa4.mazerunner;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestMazeRunner {
+    @Test
+    void sampleTest() {
+        assertEquals(1, 1); // Example test
+    }
+}
