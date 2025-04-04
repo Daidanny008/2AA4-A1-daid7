257132a1feccf2dbf389307df61db89ce7464a65
Daidanny008 <59802789+Daidanny008@users.noreply.github.com>
2025-04-03 23:24:06 -0400

    added more comments, dissected right hand exploration into more sub methods and removed the need to undo functions

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/CommandPattern/PathExecutor.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/CommandPattern/PathExecutor.java
@@ -3,8 +3,10 @@ package ca.mcmaster.se2aa4.mazerunner.CommandPattern;
 import java.util.Stack;
 
 public class PathExecutor {
+    // Declare a stack to keep track of undoable commands
     private Stack<Undoable> undoStack = new Stack<>();
     
+    // Call execution of commands, and push to stack if undoable
     public void executeCommand(Command cmd) {
         cmd.execute();
         if (cmd instanceof Undoable) {
@@ -12,12 +14,14 @@ public class PathExecutor {
         }
     }
 
+    // Undo the last command
     public void undoCommand() {
         if (!undoStack.isEmpty()) {
             undoStack.pop().undo();
         }
     }
 
+    // reset the stack
     public void reset() {
         undoStack = new Stack<>();
     }
--- src/main/java/ca/mcmaster/se2aa4/mazerunner/RightHandExploration.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/RightHandExploration.java
@@ -5,7 +5,7 @@ import ca.mcmaster.se2aa4.mazerunner.CommandPattern.*;
 
 public class RightHandExploration implements explorationAlgorithm {
 
-    // declare s
+    // declare sub classes
     private Player player;
     private Maze maze;
     private PathExecutor executor; 
@@ -32,68 +32,48 @@ public class RightHandExploration implements explorationAlgorithm {
     }
 
     // Check if the player can move forward
-    private boolean canMove() {
+    private boolean canMoveForward() {
         int[] nextMove = player.predictMove(); // get player pos after a forward move
         return !maze.isWall(nextMove[0], nextMove[1]); // check if the next move position is a wall
+    }
+
+    // Check if the player can turn right
+    private boolean canMoveRight() {
+        player.turnRight();
+        boolean canMove = canMoveForward();
+        player.turnLeft(); // Return to original orientation
+        return canMove;
+    }
+    
+    // Check if the player can turn left
+    private boolean canMoveLeft() {
+        player.turnLeft();
+        boolean canMove = canMoveForward();
+        player.turnRight(); // Return to original orientation
+        return canMove;
+    }
+    
+    private boolean isAtExit() {
+        return player.getCol() == maze.getWidth() - 1;
     }
 
     @Override
     public void explore(Maze maze, Player player) {
 
         // Right hand exploration
-        boolean moved = false;
-        while (player.getCol() != maze.getWidth() - 1) { // check if player is at exit
-            moved = false;
-
-            // Check if can turn right, turn right and move forward
-            if (!moved) {
+        while (!isAtExit()) { // check if player is at exit
+            if (canMoveRight()) {
                 executor.executeCommand(turnRight); // turn right
-                if (canMove()) {
-                    // logs RF
-                    executor.executeCommand(moveForward); // move forward
-                    moved = true;
-                }
-            }
-            // check if can move forward, move forward
-            if (!moved) {
-                executor.executeCommand(turnLeft); // turn left back to forward direction
-                if (canMove()) {
-                    // logs F = RL -> '' -> F
-                    for (int i = 0; i < 2; i++) { // undo twice
-                        executor.undoCommand(); // undo
-                    }
-                    executor.executeCommand(moveForward); // move forward
-                    moved = true;
-                }
-            }
-            // check if can move left, turn left and move forward
-            if (!moved) {
-                executor.executeCommand(turnLeft); // turn left to left direction
-                if (canMove()) {
-                    // logs LF = RLL -> '' -> LF
-                    for (int i = 0; i < 3; i++) { // undo 3 times
-                        executor.undoCommand(); // undo
-                    }
-                    executor.executeCommand(turnLeft); // redo turn left
-                    executor.executeCommand(moveForward); // move forward
-
-                    moved = true;
-                }
-            }
-            // check if dead end, turn left again then move forward
-            if (!moved) {
-                executor.executeCommand(turnLeft); // turn left to backward direction
-                if (canMove()) {
-                    // logs LLF = RLLL -> '' -> LLF
-                    for (int i = 0; i < 4; i++) { // undo 4 times
-                        executor.undoCommand(); // undo
-                    }
-                    executor.executeCommand(turnLeft); // redo turn left twice
-                    executor.executeCommand(turnLeft); 
-                    executor.executeCommand(moveForward); // move forward
-
-                    moved = true;
-                }
+                executor.executeCommand(moveForward); // move forward
+            } else if (canMoveForward()) {
+                executor.executeCommand(moveForward); // move forward
+            } else if (canMoveLeft()) {
+                executor.executeCommand(turnLeft); // turn left 
+                executor.executeCommand(moveForward); // move forward
+            } else {
+                executor.executeCommand(turnLeft); // turn left 
+                executor.executeCommand(turnLeft); // turn left 
+                executor.executeCommand(moveForward); // move forward
             }
         }
     }
